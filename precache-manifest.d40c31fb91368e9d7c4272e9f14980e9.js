0b35af1878ad50d7cdad37cd801430a377e42fb9
solipsis-project <the.solipsis.project@gmail.com>
2022-06-24 18:04:44 -0700

    Add build to github pages

---
+++ precache-manifest.d40c31fb91368e9d7c4272e9f14980e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eff2741a80acc39da1d7",
+    "url": "/static/css/main.b58cd885.chunk.css"
+  },
+  {
+    "revision": "eff2741a80acc39da1d7",
+    "url": "/static/js/main.eff2741a.chunk.js"
+  },
+  {
+    "revision": "1d33af6033afef46898c",
+    "url": "/static/css/1.6542cc9b.chunk.css"
+  },
+  {
+    "revision": "1d33af6033afef46898c",
+    "url": "/static/js/1.1d33af60.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d146d5ec75e74574b7f5fb73dafca20",
+    "url": "/index.html"
+  }
+];
